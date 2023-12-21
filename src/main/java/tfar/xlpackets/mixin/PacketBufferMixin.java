9f965b830f24d906eae30a100f26c3a2fb353dd3
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-12-21 10:35:14 -0800

    I found the issue :facepalm:

--- src/main/java/tfar/xlpackets/mixin/PacketBufferMixin.java
+++ src/main/java/tfar/xlpackets/mixin/PacketBufferMixin.java
@@ -1,26 +1,14 @@
 package tfar.xlpackets.mixin;
 
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtAccounter;
 import net.minecraft.network.FriendlyByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
-
-import javax.annotation.Nullable;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(FriendlyByteBuf.class)
-public abstract class PacketBufferMixin {
-    @Shadow(remap = false)
-    public abstract CompoundTag readNbt(NbtAccounter p_130082_);
-
-    /**
-     * @author RedstoneWizard08
-     * @reason Raise packet buffer limit
-     */
-    @Nullable
-    @Overwrite(remap = false)
-    public CompoundTag readNbt() {
-        return this.readNbt(new NbtAccounter(2_000_000_000L));
+public class PacketBufferMixin {
+    @ModifyConstant(method = "readNbt()Lnet/minecraft/nbt/CompoundTag;", constant = @Constant(longValue = 2097152L))
+    private long xlPackets(long constant) {
+        return 2_000_000_000L;
     }
 }
--- src/main/java/tfar/xlpackets/mixin/PacketInflaterMixin.java
+++ src/main/java/tfar/xlpackets/mixin/PacketInflaterMixin.java
@@ -1,61 +1,14 @@
 package tfar.xlpackets.mixin;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.DecoderException;
 import net.minecraft.network.CompressionDecoder;
-import net.minecraft.network.FriendlyByteBuf;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
-
-import java.util.List;
-import java.util.zip.Inflater;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(CompressionDecoder.class)
 public class PacketInflaterMixin {
-	@Shadow(remap = false)
-	private boolean validateDecompressed;
-
-	@Shadow(remap = false)
-	private int threshold;
-
-	@Final
-	@Shadow(remap = false)
-	private Inflater inflater;
-
-	/**
-	 * @author RedstoneWizard08
-	 * @reason Raise packet inflater limit to 2000000000
-	 */
-	@Overwrite(remap = false)
-	protected void decode(ChannelHandlerContext p_129441_, ByteBuf p_129442_, List<Object> p_129443_) throws Exception {
-		if (p_129442_.readableBytes() != 0) {
-			FriendlyByteBuf friendlybytebuf = new FriendlyByteBuf(p_129442_);
-			int i = friendlybytebuf.readVarInt();
-			if (i == 0) {
-				p_129443_.add(friendlybytebuf.readBytes(friendlybytebuf.readableBytes()));
-			} else {
-				if (this.validateDecompressed) {
-					if (i < this.threshold) {
-						throw new DecoderException("Badly compressed packet - size of " + i + " is below server threshold of " + this.threshold);
-					}
-
-					if (i > 2000000000) {
-						throw new DecoderException("Badly compressed packet - size of " + i + " is larger than protocol maximum of 2000000000");
-					}
-				}
-
-				byte[] abyte = new byte[friendlybytebuf.readableBytes()];
-				friendlybytebuf.readBytes(abyte);
-				this.inflater.setInput(abyte);
-				byte[] abyte1 = new byte[i];
-				this.inflater.inflate(abyte1);
-				p_129443_.add(Unpooled.wrappedBuffer(abyte1));
-				this.inflater.reset();
-			}
-		}
+	@ModifyConstant(method = "decode",constant = @Constant(intValue = 8388608))
+	private int xlPackets(int old) {
+		return 2000000000;
 	}
 }
